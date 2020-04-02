78a7a71bb17f33104f3eb18e59a05e6e38fdaf20
M. Clisman <matheusclisman@live.com>
2020-04-02 04:25:06 -0400

    Update functions.py
    Acrescentando opção as funções. Onde posso escolher se desejo ou não letras maiusculas ou não.

--- functions.py
+++ functions.py
@@ -11,3 +11,20 @@ last_ = input('Enter your last name: ')
 last_inicial = get_initial(last_)
 
 print('Your initial is: ' + first_inicial + last_inicial)
+
+# Dando opção de upper()
+
+def get_inicial(nome, opção_uppercase):
+    if opção_uppercase:
+        inicial = nome[0:1].upper()
+    else:
+        inicial = nome[0:1]
+    return inicial
+
+primeiro_nome = input('Entre com o primeiro nome: ')
+inicial_primeiro_nome = get_inicial(nome=primeiro_nome, opção_uppercase=True)
+
+segundo_nome = input('Entre com o ultimo nome: ')
+inicial_segundo_nome = get_inicial(nome=segundo_nome, opção_uppercase=True)
+
+print('Suas iniciais são: ' + inicial_primeiro_nome + inicial_segundo_nome)
