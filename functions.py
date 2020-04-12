cec7dace494cdae98f855c8aeb6efb0da2ddf68d
M. Clisman <matheusclisman@live.com>
2020-04-12 14:49:03 -0400

    adicionando recursos
    Adicionando recursos, decrições exemplos de como funciona uma função

--- functions.py
+++ functions.py
@@ -1,5 +1,34 @@
 # Aprendendo a usar funções
 
+
+# ||    Posso fazer uma função sem passar argumentos, tendo uma função fechada
+
+def mult_two_add_three(): # aqui, a função não tem argumentos, parametros. Então nela a variavel será number = 5 apenas. Assim, tenho uma função fechada
+    number = 5
+    print(number*2 + 3)
+
+
+# Chamando a função
+mult_two_add_three() # a função é chamada, porem sem parametros
+
+
+# ||    Para tornar a função mais útil, posso passar argumentos, parametros para este função. Assim, ao invés de ter uma variavel com um valor fixo, posso passar valor a esta função
+
+def mult_two_add_three(number): # na função, passei como parametro a variavel number. Assim, quando chamar a função, passarei o valor que quero que number assuma
+    print(number*2 + 3)
+
+mult_two_add_three(1) # neste caso, o para o parametro/argumento da função, estou passando o valor 1. Assim, nummber assumira o valor 1
+
+
+#   ||    # Passando por uma variavel o valor do parametro que a função assumira
+def mult(number):
+    print(number*2 + 3)
+
+x = int(input('Informe o valor: '))
+mult(x)
+
+
+                                                               #######################################
 def get_initial(name):
     initial = name[0:1]
     return initial
---
+++ test.py
@@ -0,0 +1,5 @@
+def mult(number):
+    print(number*2 + 3)
+
+x = int(input('Informe o valor: '))
+mult(x)
