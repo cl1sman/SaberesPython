1501a2161c1a472c7063ad4bd1ada331dfc2a188
cl1sman <matheusclisman@live.com>
2020-04-19 02:36:32 -0400

    using + to add item in list

---
+++ List/list_add_with_(+).py
@@ -0,0 +1,11 @@
+name = ['clisman', 'jorge']
+age = [32, 45]
+
+add_name = input('Enter a name: ')
+name = name + [add_name]
+
+add_age = int(input('Enter age: '))
+age = age + [add_age]
+
+print(name)
+print(age)
