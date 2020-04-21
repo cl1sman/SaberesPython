8f4bdb59542a62dfa033c52d37f3987aab24f95e
Clisman <matheusclisman@live.com>
2020-04-21 15:17:45 -0400

    test

--- test.py
+++
@@ -1,7 +0,0 @@
-def greet_customer(grocery_store, special_item):
-    print('Welcome to ' + grocery_store + '.')
-    print('Our special is ' + special_item + '.')
-    print('Have fun shopping')
-
-x = int(input('Informe o valor: '))
-mult(x)greet_customer('Naruto doces', 'Hakuna Matata')
