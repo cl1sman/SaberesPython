c853282b44b9e2f01f75d9ea13146aca4990dedf
Clisman <matheusclisman@live.com>
2020-06-10 18:44:13 -0400

    organizer

--- Projects/Basta Fazoolin'/input_menus.py
+++ Projects/Basta Fazoolin'/input_menus.py
@@ -1,4 +1,6 @@
 from class_menus import Business, Franchise, Menu # Poderia ter usado * para dizer que era pra importar todos
+# from class_menus import *
+
 # Brunch Menu
 brunch_items = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
