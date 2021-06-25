5b3a3f56a724e23d941bef52ca9a62c15832697e
Clisman <matheusclisman@live.com>
2021-06-25 18:09:41 -0400

    trabalho sem nome

--- Courses/Alg-Prog/Trabalho/T2.py
+++ Courses/Alg-Prog/Trabalho/T2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# Matheus Clisman Mariano da Silva
-# Trabalho 2
-# Professor(a): Diego Padilha Rubert
-#
+
 
 ####################################################
 # input:                                           #
