fdfbadad10f58f3923638f72efef71d7431ecf9d
Matheus Clisman <matheusclisman@live.com>
2022-02-15 22:28:54 -0400

    fix format

--- Courses/100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp_for_2022/Day_2/tip_calculator.py
+++ Courses/100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp_for_2022/Day_2/tip_calculator.py
@@ -11,4 +11,4 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 split = int(input("How many people to split the bill? "))
 
-print(f"Each person should pay: $ {round((bill *  ((tip/100) + 1)) / split, 2)}")
+print(f"Each person should pay: $ {round((bill *  ((tip/100) + 1)) / split, 2):.2f}") # fix 33.6 =>  add :.2f
