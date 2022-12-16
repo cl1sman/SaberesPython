8594f26b27681b279613dd0d8d3fcd1c2435076e
Matheus Clisman <matheusclisman@live.com>
2022-12-15 23:25:16 -0400

    change print, using max()

--- Courses/100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp_for_2022/Day_9/blind_auction.py
+++ Courses/100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp_for_2022/Day_9/blind_auction.py
@@ -3,11 +3,24 @@ import os
 continueWhileYes = True
 bids = {}
 while continueWhileYes:
-    name = input('What is your name? ')
-    valor = float(input('What\'s your bid?:$ '))
+    name = input('What is your name?\n')
+    valor = float(input('What\'s your bid?:\n$'))
     bids[name] = valor
     question = input('Are there any other bidders? Type \'yes\' or \'no\'\n')
-    if question == 'no':
+    if question.lower() == 'no':
         continueWhileYes = False
     os.system('clear')
-print('The great lance is ' + max(bids) + ': $' + str(bids[max(bids)]))
+# greater = 0
+# for i in bids:
+#     if bids[i] > greater:
+#         greater = bids[i]
+#         win = i
+# print('Greater bids ' + win + ' $' + str(greater))
+
+# other way, no need comparation
+# max_value = max(bids.values()) # value
+# max_key = max(bids, key=bids.get) # key
+# print(f'The greater is {max_key}: ${max_value}')
+for i in bids:
+    print(f'{i}: {bids[i]}')
+print(f'The highest bidder was {max(bids, key=bids.get)}: ${max(bids.values())}')
